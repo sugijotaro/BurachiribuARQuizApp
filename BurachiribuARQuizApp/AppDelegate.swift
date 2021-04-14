96eb7fcb325378316bf378b3b13c954a2ac4b17f
sugijotaro <sugi.jotaro@gmail.com>
2021-04-14 18:12:37 +0900

    Firebase、Crashlytics導入
    gitignoreを修正
    
    googleServiceinfo消した

--- BurachiribuARQuizApp/AppDelegate.swift
+++ BurachiribuARQuizApp/AppDelegate.swift
@@ -7,15 +7,16 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
@@ -41,14 +42,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
-    private func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-
-        //フォント一括変更
-        
-        return true
-    }
-
-
 }
 
