b63ede82400e2071360f3544d5de954fb2d49b9e
sugijotaro <sugi.jotaro@gmail.com>
2021-09-20 12:28:15 +0900

    未使用の関数を削除

--- BurachiribuARQuizApp/CustomClass.swift
+++ BurachiribuARQuizApp/CustomClass.swift
@@ -24,29 +24,3 @@ import UIKit
         super.draw(rect)
     }
 }
-
-
-@IBDesignable class CustomButton: UIButton {
-    
-    // 角丸の半径(0で四角形)
-    @IBInspectable var cornerRadius: CGFloat = 0.0
-    
-    // 枠
-    @IBInspectable var borderColor: UIColor = UIColor.clear
-    @IBInspectable var borderWidth: CGFloat = 0.0
-    
-    override func draw(_ rect: CGRect) {
-        // 角丸
-        self.layer.cornerRadius = cornerRadius
-        self.clipsToBounds = (cornerRadius > 0)
-        
-        // 枠線
-        self.layer.borderColor = borderColor.cgColor
-        self.layer.borderWidth = borderWidth
-        
-        //改行
-        titleLabel?.numberOfLines = 0
-        
-        super.draw(rect)
-    }
-}
