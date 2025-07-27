f8ecaf4f97d681373bf7b440130f9af895cb992b
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2025-07-27 13:44:22 +0900

    pod remove

--- Pods/Target Support Files/Pods-BurachiribuARQuizApp-BurachiribuARQuizAppUITests/Pods-BurachiribuARQuizApp-BurachiribuARQuizAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BurachiribuARQuizApp_BurachiribuARQuizAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_BurachiribuARQuizApp_BurachiribuARQuizAppUITests
-@end
